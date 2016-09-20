f44c195afe0bafa7f763956f8e5f268558b622a8
ccliu <ccliu@nuvoton.com>
2016-09-20 15:42:14 +0800

    使用jqGrid元件，做為gridivew

--- MVCDemo/App_Start/BundleConfig.cs
+++ MVCDemo/App_Start/BundleConfig.cs
@@ -9,10 +9,20 @@ namespace MVCDemo
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js"
+                      ));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
+                        "~/Scripts/jquery-ui-{version}.js"
+                      ));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryjqgrid").Include(
+                     "~/Scripts/jquery.jqgrid.js"
+                     , "~/Scripts/i18n/grid.locale-tw.js"
+                   ));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
@@ -25,7 +35,29 @@ namespace MVCDemo
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery.jqgrid/ui.jqgrid.css"));
+
+            bundles.Add(new StyleBundle("~/Content/themes/base").Include(
+                   //"~/Content/themes/base/jquery-ui.css",
+                   //"~/Content/themes/base/jquery-ui.accordion.css",
+                   //"~/Content/themes/base/jquery-ui.all.css"
+                   //, "~/Content/themes/base/jquery-ui.autocomplete.css"
+                    "~/Content/themes/base/all.css"
+                   //, "~/Content/themes/base/jquery-ui.button.css"
+                   //, "~/Content/themes/base/jquery-ui.core.css"
+                   //, "~/Content/themes/base/jquery-ui.datepicker.css"
+                   //, "~/Content/themes/base/jquery-ui.dialog.css"
+                   //, "~/Content/themes/base/jquery-ui.menu.css"
+                   //, "~/Content/themes/base/jquery-ui.progressbar.css"
+                   //, "~/Content/themes/base/jquery-ui.resizable.css"
+                   //, "~/Content/themes/base/jquery-ui.selectable.css"
+                   //, "~/Content/themes/base/jquery-ui.slider.css"
+                   //, "~/Content/themes/base/jquery-ui.spinner.css"
+                   //, "~/Content/themes/base/jquery-ui.tabs.css"
+                   //, "~/Content/themes/base/jquery-ui.theme.css"
+                   //, "~/Content/themes/base/jquery-ui.tooltip.css"
+                   ));
         }
     }
 }
---
+++ MVCDemo/Controllers/MobileController.cs
@@ -0,0 +1,67 @@
+﻿using MVCDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class MobileController : Controller
+    {
+        testDBEntities db = new testDBEntities();
+
+        // GET: Mobile
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult myDataGrid()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public JsonResult getMobileInfo(string _search, int? page, int? rows, string sord)
+        {
+
+            var result = db.Mobile20160830.ToList();
+
+            int pageSize = rows.HasValue ? rows.Value : 10;
+            int pageNum = page.HasValue ? page.Value : 1;
+            int totalRecords = result.Count;
+            int totalPages = (int)Math.Ceiling((float)totalRecords / (float)pageSize);
+
+            var jsonData = new
+            {
+                total = totalPages,
+                page = pageNum,
+                records = totalRecords,
+                rows = result.Skip((pageNum - 1) * pageSize).Take(pageSize)
+            };
+
+
+            //var jsonData = new
+            //{
+            //    total = 1,
+            //    page = 1,
+            //    records = db.Mobile20160830.ToList().Count,
+            //    rows = (
+            //      from mob in db.Mobile20160830.ToList()
+            //      select new
+            //      {
+            //          id = mob.id,
+
+            //          deviceos = mob.DeviceOS,
+            //          devicetype = mob.DeviceType,
+            //          userdisplayname = mob.UserDisplayName
+
+
+            //      }).ToArray()
+            //};
+
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
